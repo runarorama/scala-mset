37d1045d10c213e12543e88a004a01c8a7480f6a
Runar Bjarnason <runarorama@gmail.com>
2018-03-20 22:22:23 -0400

    .

--- src/main/scala/mset/MSet.scala
+++ src/main/scala/mset/MSet.scala
@@ -1,28 +1,46 @@
 package mset
 
 import spire.syntax.monoid._
+import spire.syntax.additiveMonoid._
 import spire.syntax.eq._
 import spire.algebra.Monoid
 import spire.algebra.Eq
-import spire.algebra.Ring
+import spire.algebra.MultiplicativeMonoid
+import spire.algebra.AdditiveMonoid
 import spire.math.Natural
 import spire.math.Rational
 import spire.std.seq._
 import spire.std.map._
 import spire.std.tuples._
 
-case class MSet[M,A](rep: Map[A,M]) {
+class MSet[M,A](val rep: Map[A,M]) extends AnyVal {
+
+  import MSet._
+
+  /** Get the occurrence list of this MSet */
   def occurList(implicit E: Eq[M], M: Monoid[M]): List[(A,M)] =
     rep.toList.filter { case (_, m) => !m.isEmpty }
 
+  /** Get the multiplicity of a given object */
   def apply(a: A)(implicit M: Monoid[M]): M =
     rep get a getOrElse M.empty
 
+  /** An object occurs in the MSet if its multiplicity is nonzero */
   def contains(a: A)(implicit E: Eq[M], M: Monoid[M]): Boolean =
     !apply(a).isEmpty
 
+  /** `a` is from `b` when every element of `a` also occurs in `b` */
   def isFrom(m: MSet[M,A])(implicit E: Eq[M], M: Monoid[M]): Boolean =
     rep.foldLeft(true) { case (b, (a,_)) => b && (m contains a) }
+
+  /** Fold an MSet with a monoid */
+  def foldMap[B](f: (A,M) => B)(implicit B: Monoid[B]): B =
+    rep.foldLeft(B.empty) { case (b, (a, m)) => b |+| f(a, m) }
+
+  def insert(a: A)(implicit M: MultiplicativeMonoid[M], S: AdditiveMonoid[M]): MSet[M,A] = {
+    implicit val MM = M.multiplicative
+    new MSet(MapMonoid[A,M].combine(rep, singleton[M,A](a).rep))
+  }
 }
 
 object MSet {
@@ -36,10 +54,22 @@ object MSet {
       a.occurList === b.occurList
   }
 
+  implicit def msetMonoid[M:Monoid,A]: Monoid[MSet[M,A]] =
+    new Monoid[MSet[M,A]] {
+      def combine(a: MSet[M,A], b: MSet[M,A]) =
+        new MSet(a.rep |+| b.rep)
+      val empty = MSet.empty[M,A]
+    }
+
+  /** Turn an occurrence list into an MSet */
   def fromList[M,A](xs: List[(A,M)])(implicit M: Monoid[M]): MSet[M,A] =
-    MSet(xs.foldLeft(Map.empty:Map[A,M]) { case (s, (a, m)) => s |+| Map(a -> m) })
+    new MSet(xs.foldLeft(Map.empty:Map[A,M]) { case (s, (a, m)) => s |+| Map(a -> m) })
 
-  def empty[M,A]: MSet[M,A] = MSet(Map.empty)
+  /** The empty mset */
+  def empty[M,A]: MSet[M,A] = new MSet(Map.empty)
 
-  def singleton[M,A](a: A)(implicit M: Ring[M]): MSet[M,A] = MSet(Map(a -> M.one))
+  /** Construct an mset where the given element occurs once */
+  def singleton[M,A](a: A)(implicit M: MultiplicativeMonoid[M]): MSet[M,A] =
+    new MSet(Map(a -> M.one))
+
 }
